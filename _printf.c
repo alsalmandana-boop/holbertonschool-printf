7b5db44ceb4c10308f6b1871b1594c059444214e
Dana Alsalman <alsalmandana@gmail.com>
2026-08-12 17:38:46 +0300

    Enhance _printf with error handling and readability
    Updated the _printf function to include error handling for the printed variable and improved code readability.

--- _printf.c
+++ _printf.c
@@ -4,47 +4,70 @@
  * _printf - Produces output according to a format
  * @format: Format string
  *
- * Return: Number of characters printed
+ * Return: Number of characters printed, or -1 on failure
  */
-
 int _printf(const char *format, ...)
 {
-    va_list args;
-    int i = 0;
-    int count = 0;
+	va_list args;
+	int i;
+	int count;
+	int printed;
 
-    if (format == NULL)
-        return (-1);
+	if (format == NULL)
+		return (-1);
 
-    va_start(args, format);
+	va_start(args, format);
+	i = 0;
+	count = 0;
 
-    while (format[i] != '\0')
-    {
-        if (format[i] != '%')
-        {
-            write(1, &format[i], 1);
-            count++;
-        }
-        else
-        {
-            i++;
+	while (format[i] != '\0')
+	{
+		if (format[i] != '%')
+		{
+			printed = print_char(format[i]);
+		}
+		else
+		{
+			i++;
 
-            if (format[i] == 'c')
-                count += print_char(va_arg(args, int));
-            else if (format[i] == 's')
-                count += print_string(va_arg(args, char *));
-            else if (format[i] == '%')
-                count += print_char('%');
-            else if (format[i] == 'd')
-                count += print_number(va_arg(args, int));
-            else if (format[i] == 'i')
-                count += print_number(va_arg(args, int));
-        }
+			if (format[i] == '\0')
+			{
+				va_end(args);
+				return (-1);
+			}
 
-        i++;
-    }
+			if (format[i] == 'c')
+				printed = print_char((char)va_arg(args, int));
+			else if (format[i] == 's')
+				printed = print_string(va_arg(args, char *));
+			else if (format[i] == '%')
+				printed = print_char('%');
+			else if (format[i] == 'd' || format[i] == 'i')
+				printed = print_number(va_arg(args, int));
+			else
+			{
+				printed = print_char('%');
+				if (printed == -1)
+				{
+					va_end(args);
+					return (-1);
+				}
 
-    va_end(args);
+				count += printed;
+				printed = print_char(format[i]);
+			}
+		}
 
-    return (count);
+		if (printed == -1)
+		{
+			va_end(args);
+			return (-1);
+		}
+
+		count += printed;
+		i++;
+	}
+
+	va_end(args);
+	return (count);
 }
